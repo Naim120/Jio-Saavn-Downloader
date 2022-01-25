a4b4540cccd4771ae5d7ce53d1e5b7613e531497
sk naim <sknaimuddin234@gmail.com>
2022-01-25 23:50:06 +0530

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,3 +1,3 @@
 const API_CONFIG = {
-    url: 'http://4a9465ef8477.ngrok.io' // Don't give last slash ('/') https://github.com/cachecleanerjeet/jiosaavnapi
+    url: 'http://50f9-18-218-145-166.ngrok.io' // Don't give last slash ('/') https://github.com/cachecleanerjeet/jiosaavnapi
 }
